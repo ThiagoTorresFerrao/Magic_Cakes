1cb16d815946f4d3f589b09949d418f31180ae6b
Thiago Torres <137134010+ThiagoTorresFerrao@users.noreply.github.com>
2025-06-14 20:57:13 -0300

    Update Files
    Upade all files.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "home": "url('/assets/WhatsApp Image 2024-11-23 at 09.47.11.jpeg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
